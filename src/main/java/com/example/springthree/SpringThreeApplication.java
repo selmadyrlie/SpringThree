e2c3d1824a6a5a9a895dc1c90bf8d682e4685d08
selmadyrlie <sejoh0292@oslomet.no>
2024-04-04 09:25:38 +0200

    wrote most of the code

---
+++ src/main/java/com/example/springthree/SpringThreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springthree/SpringThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
